1ad73889ff9210d602670e83981f0d1dfcbd7b5d
Aslı <83712808+asli-inanc@users.noreply.github.com>
2021-05-07 23:29:27 +0300

    Update SearchPageContants.java

--- gittigidiyorOtomasyon-master/src/test/java/Contants/SearchPageContants.java
+++ gittigidiyorOtomasyon-master/src/test/java/Contants/SearchPageContants.java
@@ -8,9 +8,9 @@ public interface SearchPageContants {
 
 
 
-     ElementInfo searchInput = new ElementInfo("E-posta adresi girilemedi", By.xpath("//div[@class=\"sc-4995aq-4 dNPmGY\"]/input"));
-     ElementInfo secondPageButton = new ElementInfo("İkinci Sayfaya gidilemedi", By.xpath("//ul[@class='clearfix']/li/a[.='2']"));
-     ElementInfo secondPageActiveControl = new ElementInfo("İkinci sayfa aktif değil", By.xpath("//li[@class='selected']/a[.='2']"));
-     ElementInfo addBasketButton = new ElementInfo("Sepete eklenemedi", By.xpath("//button[@id='add-to-basket']"));
+     ElementInfo searchInput = new ElementInfo("E-mail address could not be entered", By.xpath("//div[@class=\"sc-4995aq-4 dNPmGY\"]/input"));
+     ElementInfo secondPageButton = new ElementInfo("Could not go to second page", By.xpath("//ul[@class='clearfix']/li/a[.='2']"));
+     ElementInfo secondPageActiveControl = new ElementInfo("The second page is not active", By.xpath("//li[@class='selected']/a[.='2']"));
+     ElementInfo addBasketButton = new ElementInfo("Could not add to cart", By.xpath("//button[@id='add-to-basket']"));
 
 }
