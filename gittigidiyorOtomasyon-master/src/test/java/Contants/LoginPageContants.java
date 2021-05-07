af22fde344c73e5f795bd67e6814d86b18ed398f
Aslı <83712808+asli-inanc@users.noreply.github.com>
2021-05-07 23:27:39 +0300

    Update LoginPageContants.java

--- gittigidiyorOtomasyon-master/src/test/java/Contants/LoginPageContants.java
+++ gittigidiyorOtomasyon-master/src/test/java/Contants/LoginPageContants.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.By;
 import util.ElementInfo;
 
 public interface LoginPageContants {
-     ElementInfo login = new ElementInfo("Giriş yapılamadı", By.xpath("//*[@id=\"main-header\"]/div[3]/div/div/div/div[3]/div/div[1]/div"));
-     ElementInfo loginbutton = new ElementInfo("Giriş butonu bulunamadı", By.xpath("//div[@class='sc-12t95ss-3 fDarBX']/a[@href=\"https://www.gittigidiyor.com/uye-girisi\"]"));
-     ElementInfo emailInput = new ElementInfo("E-posta adresi girilemedi", By.id("L-UserNameField"));
-     ElementInfo passInput = new ElementInfo("E-posta şifresi girilemedi", By.id("L-PasswordField"));
-     ElementInfo singIn = new ElementInfo("Giriş yapılamadı", By.id("gg-login-enter"));
-     ElementInfo submitCheck= new ElementInfo("Hesabın elementi gözükmedi", By.xpath("//span[.='asliinanc178591']"));
+     ElementInfo login = new ElementInfo("Could not login", By.xpath("//*[@id=\"main-header\"]/div[3]/div/div/div/div[3]/div/div[1]/div"));
+     ElementInfo loginbutton = new ElementInfo("Login button not found", By.xpath("//div[@class='sc-12t95ss-3 fDarBX']/a[@href=\"https://www.gittigidiyor.com/uye-girisi\"]"));
+     ElementInfo emailInput = new ElementInfo("E-mail address could not be entered", By.id("L-UserNameField"));
+     ElementInfo passInput = new ElementInfo("E-mail password could not be entered", By.id("L-PasswordField"));
+     ElementInfo singIn = new ElementInfo("Could not login", By.id("gg-login-enter"));
+     ElementInfo submitCheck= new ElementInfo("Account element not visible", By.xpath("//span[.='asliinanc178591']"));
 
 
 }
