e67c32233fc7797f5271a85215e49703ed0b4f44
Aslı <83712808+asli-inanc@users.noreply.github.com>
2021-05-07 23:25:08 +0300

    Update BasketPageContants.java

--- gittigidiyorOtomasyon-master/src/test/java/Contants/BasketPageContants.java
+++ gittigidiyorOtomasyon-master/src/test/java/Contants/BasketPageContants.java
@@ -6,9 +6,9 @@ import util.ElementInfo;
 public interface BasketPageContants {
 
 
-    ElementInfo basketButton = new ElementInfo("Sepette eklenemedi", By.cssSelector(".gg-ui-btn-default.padding-none"));
-    ElementInfo productAreaSelect = new ElementInfo("Urun seçilemedi", By.className("gg-input-select"));
-    ElementInfo deleteProduct = new ElementInfo("Urun Silinemedi", By.className("gg-icon-bin-medium"));
-    ElementInfo basketControl = new ElementInfo("Sepet kontrol edilemedi", By.xpath("//h2[.='Sepetinizde ürün bulunmamaktadır.']"));
+    ElementInfo basketButton = new ElementInfo("Could not add to cart", By.cssSelector(".gg-ui-btn-default.padding-none"));
+    ElementInfo productAreaSelect = new ElementInfo("the product could not be selected", By.className("gg-input-select"));
+    ElementInfo deleteProduct = new ElementInfo("The product could not be deleted", By.className("gg-icon-bin-medium"));
+    ElementInfo basketControl = new ElementInfo("Cart could not be checked", By.xpath("//h2[.='Sepetinizde ürün bulunmamaktadır.']"));
 
 }
